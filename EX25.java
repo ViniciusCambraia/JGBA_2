20d8f0c373c7caaa0524eddc1295aa42a9ab4fce
Vinicius Cambraia <53883408+ViniciusCambraia@users.noreply.github.com>
2019-08-07 22:27:41 -0300

    Update EX25.java

--- EX25.java
+++ EX25.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
 
 public class EX25 {
+	
+	static Scanner entrada;
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		entrada = new Scanner(System.in);
+		int n;
+		
+		System.out.print("Digite um numero inteiro: ");
+		n = Integer.parseInt(entrada.nextLine());
+		
+		if (n%2 == 0)
+		System.out.println("O númeroinformado é par ");
+		else
+			System.out.println("o número informado é impar");
+		
+		entrada.close();
+	}
 }
